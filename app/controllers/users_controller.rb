9f83a3811bb77780e9693ec1eb7d3fff7755d58a
erikyoung <erik.young@rmit.edu.vn>
2017-06-21 20:33:30 +0700

    added create logic to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,23 @@ class UsersController < ApplicationController
   def index
     @users = User.all.shuffle
   end
+
+  def create
+  	@user = User.new(user_params)
+  	 if @user = User.save
+  	 	flash[:success] = "Got it bro!"
+  	 	session[:user_id] = @user.id
+  	 	redirect_to root_path
+  	 else
+  	 	flash[:error] = "Error: #{@user.errors.full_messages.to_sentence}"
+  	 	render 'new'
+  end
+end
+
+private
+
+def user_params 
+	params.require(:user).permit(:name, :email, :password, :image_url)
+end
+
 end
